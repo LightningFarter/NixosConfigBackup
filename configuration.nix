14cd2729b23852df5d8dea26d82a945d020c2bed
LightningFarter <jason5738867@gmail.com>
2025-08-02 16:31:38 +0800

    try table extra and chewing

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,8 @@
   i18n.inputMethod.enabled = "fcitx5";
   i18n.inputMethod.fcitx5.addons = with pkgs; [
     fcitx5-chinese-addons
+    fcitx5-table-extra
+    fcitx5-chewing
     fcitx5-gtk
     fcitx5-configtool
   ];
